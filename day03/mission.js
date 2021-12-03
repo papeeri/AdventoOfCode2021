3aa64ad2f761028981f8a849a6c907cef53b059e
P-A Eriksson <p-a@peeri.se>
2021-12-03 09:58:36 +0100

    day 3 part 1

--- day03/mission.js
+++ day03/mission.js
@@ -3,26 +3,42 @@ const fs = require('fs');
 let input = fs.readFileSync('./day03/input.txt', 'utf8').split(/\r?\n/);
 
 function part1(input) {
-    let horizontalPosition = 0;
-    let depth = 0;
+    let mostCommon = '';
+    let leastCommon = '';
 
-    for (let i = 0; i < input.length; i++) {
-        if (getDirection(input[i]) === 'forward') {
-            horizontalPosition = horizontalPosition + getUnit(input[i]);
+    for (let position = 0; position < 12; position++) {
+        let ones = 0;
+        let zeros = 0;
+
+        for (let i = 0; i < input.length; i++) {
+            if (input[i].split('')[position] === '1') {
+                ones++;
+            }
+
+            if (input[i].split('')[position] === '0') {
+                zeros++;
+            }
         }
 
-        if (getDirection(input[i]) === 'up') {
-            depth = depth - getUnit(input[i]);
-        }
-
-        if (getDirection(input[i]) === 'down') {
-            depth = depth + getUnit(input[i]);
+        if (ones > zeros) {
+            mostCommon += '1';
+            leastCommon += '0';
+        } else {
+            mostCommon += '0';
+            leastCommon += '1';
         }
     }
 
-    console.log('Horizontal position:', horizontalPosition);
-    console.log('Depth:', depth);
-    console.log('Horizontal position * Depth:', horizontalPosition * depth);
+    console.log('mostCommon:', mostCommon);
+    console.log('leastCommon:', leastCommon);
+
+    let gammaRate = parseInt(mostCommon, 2);
+    let epsilonRate = parseInt(leastCommon, 2);
+
+    console.log('gammaRate:', gammaRate);
+    console.log('epsilonRate:', epsilonRate);
+
+    console.log('Power consumption:', gammaRate * epsilonRate);
 }
 
 part1(input);
