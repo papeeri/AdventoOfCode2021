238d5615f4f964acceaeb87f629e7d1c86a42af4
P-A Eriksson <p-a@peeri.se>
2021-12-05 22:14:39 +0100

    prep for tomorrow

---
+++ day05/missionPart1.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const internal = require('stream');
+
+let input = fs.readFileSync('./day05/input.txt', 'utf8').split(/\r?\n/);
+
+function part1(input) {}
+
+part1(input);
