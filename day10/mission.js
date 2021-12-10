d35ebc5dc5bfc86c0e00ad15e8313a4bdb1a7151
P-A Eriksson <p-a@peeri.se>
2021-12-11 00:12:42 +0100

    solve day 10 part 1

--- day10/mission.js
+++ day10/mission.js
@@ -9,8 +9,49 @@ function parser(inputData) {
 function mission() {
     let input = getInputData(_inputPath)(parser);
 
-    console.log(input);
-    console.log("End");
+    const points = new Map([
+        [")", 3],
+        ["]", 57],
+        ["}", 1197],
+        [">", 25137],
+    ])
+
+    const closingCharMap = new Map([
+        ["(", ")"],
+        ["[", "]"],
+        ["{", "}"],
+        ["<", ">"],
+    ])
+     
+    let found = {
+       ")": 0,
+       "]": 0,
+       "}": 0,
+       ">": 0,
+    }
+
+    for (let inputRow of input) {
+        let stack = []
+        let inputRowArray = inputRow.split("")
+
+        for (let chr of inputRowArray) {
+            if (chr === "(" || chr === "[" || chr === "{" || chr === "<") {
+                stack.push(closingCharMap.get(chr))
+            } else {
+                let element = stack.pop()
+                if (element !== chr) {
+                    found[chr]++
+                }
+            }
+        }
+    }
+
+    let sum = 0
+    Object.keys(found).map(key => {
+        sum+=(found[key] * points.get(key))
+    })
+
+    console.log("Score:", sum )
 }
 
 mission();
